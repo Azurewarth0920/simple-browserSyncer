1b09142b7e198874dc57056787300bddcf279de6
樊沛 <fanpei920@gmail.com>
2019-04-07 00:03:17 +0900

    0.0.1 basic functions

---
+++ bin/simpleSyncer.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const main = require('../lib')
+
+const argv = require('yargs')
+  .version()
+  .option({
+    'd':{
+      alias: 'directory',
+      default: './',
+      describe: 'the directory you want to watch',
+    },
+    'p': {
+      alias: 'port',
+      default: '3000',
+      describe: 'the port you want to use',
+    }
+  })
+  .argv
+
+main({
+  port: argv.p,
+  directory: argv.d
+})
+

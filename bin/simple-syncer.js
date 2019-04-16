579be5bf877970f9dd5f70b0a4b79e126e6a06f9
樊沛 <fanpei920@gmail.com>
2019-04-16 23:08:56 +0900

    use path join instead of string connection

--- bin/simple-syncer.js
+++ bin/simple-syncer.js
@@ -4,23 +4,22 @@ const main = require('../lib')
 const path = require('path')
 
 const argv = require('yargs')
-  .version()
-  .option({
-    'd':{
-      alias: 'directory',
-      default: './',
-      describe: 'the directory you want to watch',
-    },
-    'p': {
-      alias: 'port',
-      default: '3000',
-      describe: 'the port you want to use',
-    }
-  })
-  .argv
+	.version()
+	.option({
+		'd':{
+	    alias: 'directory',
+			default: './',
+			describe: 'the directory you want to watch',
+		},
+		'p': {
+			alias: 'port',
+			default: '3000',
+			describe: 'the port you want to use',
+		}
+	}).argv
 
 main({
-  port: argv.p,
-  directory: path.resolve(argv.d)
+	port: argv.p,
+	directory: path.resolve(argv.d)
 })
 
